e05dc4c1d4d539c227dda51dbf1ce0d23dd44ef6
suvarneshkm <suvarnesh1729@gmail.com>
2022-02-11 00:11:47 +0530

    Nav

---
+++ components/layout/navbar.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import CloseFillIcon from 'remixicon-react/CloseFillIcon';
+import Menu5FillIcon from 'remixicon-react/Menu5FillIcon';
+
+
+
+const solutions = [
+  {
+    name: 'Home',
+    href: '#',
+  },
+  {
+    name: 'About',
+    href: '#',
+  },
+  {
+    name: 'Prizes',
+    href: '#',
+  },
+  {
+    name: 'Sponsors',
+    href: '#',
+  },
+]
+export default function Navbar() {
+  return (
+    <Popover className="sticky z-50 top-0 bg-dark">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center py-7 md:py-9">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="md:w-6 w-5 h-auto cursor-pointer"
+                src="./images/logo.png"
+                alt="logo"
+              />
+            </a>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <Popover.Button className=" rounded-md p-2 inline-flex items-center justify-center text-accent">
+              <span className="sr-only">Open menu</span>
+              <Menu5FillIcon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden md:flex space-x-16">
+            {solutions.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="button text-light hover:text-accent cursor-pointer">
+                {item.name}
+              </a>
+            ))}
+          </Popover.Group>
+        </div>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+          <div className="rounded-lg shadow-lg ring-1 ring-accent ring-opacity-5 bg-dark divide-y-2 divide-gray-50">
+            <div className="pt-5 pb-6 px-5">
+              <div className="flex items-center justify-between">
+                <div>
+                  <img
+                    className="md:w-6 w-5 h-auto cursor-pointer"
+                    src="./images/logo.png"
+                    alt="logo"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="rounded-md p-2 inline-flex items-center justify-center text-accent hover:text-light">
+                    <span className="sr-only">Close menu</span>
+                    <CloseFillIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="mt-6">
+                <nav className="grid gap-y-8">
+                  {solutions.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-m-3 p-3 flex items-center rounded-md hover:bg-light/30 text-light hover:text-accent"
+                    >
+                      <span className="ml-3 text-base font-medium">{item.name}</span>
+                    </a>
+                  ))}
+                </nav>
+              </div>
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: "'Montserrat', sans-serif",
+      },
+      colors: {
+        'dark': '#0A0A0A',
+        'light': '#F5F5F5',
+        'accent': '#F6490D',
+      },
+      fontSize: {
+        HD: ['60px', '60px'],
+        HM: ['52px', '60px'],
+        SH: ['18px', '28px'],
+        BTN: ['16px', '24px'],
+        BDY: ['18px', '28px'],
+        SBDY: ['12px', '18px'],
+      },
+      letterSpacing: {
+        'tight1': '0.0125em',
+        'tight2': '0.015em',
+        'tight3': '0.0225em',
+      },
+    },
   },
   plugins: [],
 }
