fe1254983f1c0c86339255025d366c7360d7c234
suvarneshkm <suvarnesh1729@gmail.com>
2022-02-14 01:53:11 +0530

    back to top

---
+++ components/backtotop.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import ArrowUpSLineIcon from 'remixicon-react/ArrowUpSLineIcon';
+
+
+export default function BackToTop() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+    return (
+        <div className='fixed bottom-2 right-2'>
+            <button
+                type='button'
+                onClick={scrollToTop}
+                className={
+                    isVisible ? 'opacity-100' : 'opacity-0',
+                    'inline-flex items-center p-3 rounded-full shadow-sm text-light bg-accent transition-opacity'
+                }
+            >
+                <ArrowUpSLineIcon className='h-6 w-6' aria-hidden='true' />
+            </button>
+        </div>
+    )
+}
