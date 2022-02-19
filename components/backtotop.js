0716bc213b9157f3904b39fd29f2a8c1caa14aeb
suvarneshkm <suvarnesh1729@gmail.com>
2022-02-20 01:06:00 +0530

    back to top

--- components/backtotop.js
+++ components/backtotop.js
@@ -1,9 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import ArrowUpSLineIcon from 'remixicon-react/ArrowUpSLineIcon';
+import useScrollListener from './scroll';
 
 
 export default function BackToTop() {
     const [isVisible, setIsVisible] = useState(false);
+    const [navClassList, setNavClassList] = useState([]);
+    const scroll = useScrollListener();
+    const [handleShow, setHandleShow] = useState(false);
+
+    useEffect(() => {
+        const _classList = [];
+
+        if (scroll.y > 150 && scroll.y - scroll.lastY < 0)
+            _classList.push("hidden transition duration-200 ease-out");
+
+        setNavClassList(_classList);
+    }, [scroll.y, scroll.lastY]);
+
+    useEffect(() => {
+        const listener = () => {
+            if (window.scrollY > 0) {
+                setHandleShow(true);
+            } else
+                setHandleShow(false);
+        };
+        window.addEventListener("scroll", listener);
+
+        return () => {
+            window.removeEventListener("scroll", listener);
+        };
+    }, []);
 
     const toggleVisibility = () => {
         if (window.pageYOffset > 300) {
@@ -27,7 +54,7 @@ export default function BackToTop() {
         };
     }, []);
     return (
-        <div className='fixed bottom-2 right-2'>
+        <div className={`fixed bottom-2 right-2  2xl:right-[8%] ${handleShow ? "shadow-md " : ""} ${navClassList.join(" ")}  `}>
             <button
                 type='button'
                 onClick={scrollToTop}
